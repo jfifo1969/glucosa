42e4515de6eb7e44159cba580ec0e10909224619
Julian Forero <Julian.Forero@MacBook-De-Julian.local>
2021-05-20 01:56:31 -0500

    first commit

---
+++ src/home/index.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+
+import "./styles.css";
+import Imagen from "../assets/1.png";
+
+const Home = () => {
+  const [time, setTime] = useState(Date.now());
+  const [glucosa, setGlucosa] = useState(0);
+  //   const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
+  const [data, setData] = useState([{val:100}]);
+
+  useEffect(() => {
+    const interval = setInterval(function () {
+      const temp = glucosaFunction(80, 275);
+    //   const listTemp = data
+    //   listTemp.push({ val: temp })
+    //   console.log(listTemp)
+      setData(array => [...array,{val:temp}].slice(-10));
+      return setGlucosa(temp);
+    }, 2000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  function glucosaFunction(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  return (
+    <>
+      <header>
+        <ul class="nav-right-section">
+          <a href="https://www.niddk.nih.gov/health-information/informacion-de-la-salud/diabetes/informacion-general/control#:~:text=Usted%20puede%20controlar%20su%20diabetes,llamada%20az%C3%BAcar%20en%20la%20sangre.">
+            <img src={Imagen} alt="Logo de lucha contra la diabetes" />
+          </a>
+        </ul>
+      </header>
+      <section class ="graphic-text" >
+        <h3>Tu nivel de glucosa es de: <br/>{glucosa}</h3>
+        <LineChart width={600} height={300} data={data}>
+          <Line type="monotone" dataKey="val" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" />
+          {/* <XAxis dataKey="name" /> */}
+          <YAxis />
+        </LineChart>
+      </section>
+      <footer>
+        <ul class="footer-left">
+          <li>
+            <a href="mailto:jfifo16@gmail.com?subject=Medidor continuo de glucosa&body=Solicito más información con respecto a esta plataforma.">
+              Contacto
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/login/index.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Logo from '../assets/2.jpg'
+import reportWebVitals from "../reportWebVitals";
+
+const Login = () => {
+  let history = useHistory();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  var usuarios = [
+    { nombre: "JulianForero", contraseña: "1002397054" },
+    { nombre: "DavidArevalo", contraseña: "2165551" },
+    { nombre: "JuanaFonseca", contraseña: "3167812961" },
+  ];
+
+  const validacion = () => {
+    const resp = usuarios.some((usuario) => {
+      return usuario.nombre === username && usuario.contraseña === password;
+    });
+    if (resp) {
+      history.push("/home");
+    }
+  };
+
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col ">
+          <h3>Bienvenido a su medidor continuo de Glucosa</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-md-6">
+          <div class="mb-3" id="email">
+            <label for="exampleInputEmail1" class="form-label">
+              Email address
+            </label>
+            <input
+              onChange={(val) => setUsername(val.target.value)}
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" class="form-text">
+              Recuerda nunca compartir tu email ni tu contraseña
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12 col-md-6">
+          <div class="mb-3" id="contraseña">
+            <label for="exampleInputPassword1" class="form-label">
+              Password
+            </label>
+            <input
+              onChange={(val) => setPassword(val.target.value)}
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <button
+            type="submit"
+            class="btn btn-primary"
+            onClick={() => validacion()}
+          >
+            Enviar
+          </button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-md-6 image-container">
+          <img
+            class="main-img"
+            src={Logo}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
